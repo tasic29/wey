b42cb601f6100ec748068442707dbb573509a515
Milos Tasic <tasic29@gmail.com>
2025-01-29 14:21:27 +0100

    configured jwt urls

---
+++ wey_backend/account/urls.py
@@ -0,0 +1,8 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path
+
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
